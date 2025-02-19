0be808dee843179acc6cd8c64e151bde2996a65c
luan ramos vicente <luanfrv9@gmail.com>
2025-02-19 09:08:26 -0300

    docker: fix install

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,21 +2,14 @@
 echo "\n\n"
 echo 'initializing setup for docker...'
 echo '__________________________________________________________________________'
-
-echo 'installing dependencies...'
-sudo apt install -y ca-certificates gnupg lsb-release
-
-echo 'configuring GPG keys...'
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo 'setting up remote repository...'
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-echo 'installing docker...'
-sudo apt update -y
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
-
-echo 'enabling command docker for current user...'
-sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
