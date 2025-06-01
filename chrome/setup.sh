1f7dc61d50dea7fc0c51f9d46241152311820b4d
luan ramos vicente <luanfrv9@gmail.com>
2025-06-01 11:36:26 -0300

    chrome: fix installation

--- chrome/setup.sh
+++ chrome/setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt update -y
-sudo apt install -y google-chrome-stable
+#sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#sudo apt update -y
+#sudo apt install -y google-chrome-stable
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y google-chrome-stable_current_amd64.deb
 
 # fix emojis
-sudo apt install -y fonts-noto-color-emoji
+#sudo apt install -y fonts-noto-color-emoji
