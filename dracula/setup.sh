1b8f1f3543c63fc201e41d63daa1020a28bd7089
luan ramos vicente <luanfrv9@gmail.com>
2025-06-01 12:06:43 -0300

    general updates

--- dracula/setup.sh
+++ dracula/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 mkdir -p ~/.vim/pack/plugins/start
-cp -R  ~/Documentos/dracula_pro/themes/vim ~/.vim/pack/plugins/start/dracula_pro
+cp -R  ~/Documents/dracula_pro/themes/vim ~/.vim/pack/plugins/start/dracula_pro
 mkdir -p ~/.config/alacritty
-cp ~/Documentos/dracula_pro/themes/alacritty/themes/dracula-pro.toml ~/.config/alacritty/alacritty.toml
+cp ~/Documents/dracula_pro/themes/alacritty/themes/dracula-pro.toml ~/.config/alacritty/alacritty.toml
