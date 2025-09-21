ade228d0150c1dadeb66f19edf3a9cee90e91478
luan ramos vicente <luanfrv9@gmail.com>
2025-09-21 18:50:27 -0300

    feat: setup nvim and file tree

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-sudo apt install -y neovim
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
 DOTFILES_PATH="$HOME/.dotfiles"
 MODULE_PATH="$DOTFILES_PATH/nvim"
@@ -11,3 +14,5 @@ SETUP_FILE_PATH="$SETUP_PATH/$MODULE_FILE_NAME"
 mkdir -p $SETUP_PATH
 ln -sf "$MODULE_FILE_PATH" "$SETUP_FILE_PATH"
 
+nvim +'call mkdir(stdpath("data")."/site/pack/themes/start", "p")' +q
+cp -R "$HOME/doc/dracula_pro/themes/vim" "$(nvim -es +"put =stdpath('data')" +print +'q!')"/site/pack/themes/start/dracula_pro
