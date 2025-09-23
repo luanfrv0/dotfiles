890ca1adf44edde5ea9683467bb2c9f250e311d5
luan ramos vicente <luanfrv9@gmail.com>
2025-09-23 11:48:59 -0300

    nvim: fix installation errors

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim
+sudo rm -rf /opt/nvim*
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-rm nvim-linux-x86_64.tar.gz
+rm ./nvim-linux-x86_64.tar.gz
 
 DOTFILES_PATH="$HOME/.dotfiles"
 MODULE_PATH="$DOTFILES_PATH/nvim"
