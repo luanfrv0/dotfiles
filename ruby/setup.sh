4fc3a9c71300ac54fc05ae3e88374681c7b07076
luan ramos vicente <luanfrv9@gmail.com>
2025-10-07 18:08:34 -0300

    asdf: install ruby 3.2.2

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -2,5 +2,5 @@
 sudo apt install -y autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
 source ~/.bashrc
 asdf plugin add ruby
-asdf install ruby latest
-asdf global ruby latest
+asdf install ruby 3.2.2
+asdf global ruby 3.2.2
