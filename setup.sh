350c584e6cdcbb7a161abe6218be9414530cdffb
luan ramos vicente <luanfrv9@gmail.com>
2026-03-23 18:42:44 -0300

    setup: update setup order

--- setup.sh
+++ setup.sh
@@ -4,35 +4,20 @@
 #     ./setup.sh
 #     ./setup.sh <module> (e.g ./setup.sh vim)
 #
-if [ $# -eq 0 ]; then
-  sudo apt update -y
+sudo apt update -y
 
-  . ~/.dotfiles/curl/setup.sh
-  . ~/.dotfiles/gpg/setup.sh
-  . ~/.dotfiles/git/setup.sh
-  . ~/.dotfiles/bash/setup.sh
-  . ~/.dotfiles/alacritty/setup.sh
-  . ~/.dotfiles/tmux/setup.sh
-  . ~/.dotfiles/bat/setup.sh
-  . ~/.dotfiles/asdf/setup.sh
-  . ~/.dotfiles/ibm-plex-mono/setup.sh
-  . ~/.dotfiles/docker/setup.sh
-  . ~/.dotfiles/nvim/setup.sh
-  . ~/.dotfiles/typora/setup.sh
-  . ~/.dotfiles/chrome/setup.sh
-  . ~/.dotfiles/dropbox/setup.sh
-elif [ $1 = "wsl" ];then
-  sudo apt update -y
-
-  . ~/.dotfiles/curl/setup.sh
-  . ~/.dotfiles/gpg/setup.sh
-  . ~/.dotfiles/git/setup.sh
-  . ~/.dotfiles/bash/setup.sh
-  . ~/.dotfiles/tmux/setup.sh
-  . ~/.dotfiles/bat/setup.sh
-  . ~/.dotfiles/asdf/setup.sh
-  . ~/.dotfiles/ibm-plex-mono/setup.sh
-  . ~/.dotfiles/nvim/setup.sh
-else
-  . ~/.dotfiles/$1/setup.sh
-fi
+. ~/.dotfiles/chromium/setup.sh
+. ~/.dotfiles/drivers/setup.sh
+. ~/.dotfiles/git/setup.sh
+. ~/.dotfiles/bash/setup.sh
+. ~/.dotfiles/tmux/setup.sh
+. ~/.dotfiles/docker/setup.sh
+. ~/.dotfiles/asdf/setup.sh
+. ~/.dotfiles/ruby/setup.sh
+. ~/.dotfiles/nodejs/setup.sh
+. ~/.dotfiles/nvim/setup.sh
+. ~/.dotfiles/alacritty/setup.sh
+. ~/.dotfiles/ssh/setup.sh
+. ~/.dotfiles/ang/setup.sh
+#. ~/.dotfiles/curl/setup.sh
+#. ~/.dotfiles/gpg/setup.sh
