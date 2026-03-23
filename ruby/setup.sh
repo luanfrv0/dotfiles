218fa2ab6b52d2436addc270ca1269c78cd3b9e1
luan ramos vicente <luanfrv9@gmail.com>
2026-03-23 16:08:15 -0300

    ruby: update to 3.4.5

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -2,5 +2,5 @@
 sudo apt install -y autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
 source ~/.bashrc
 asdf plugin add ruby
-asdf install ruby 3.2.2
-asdf global ruby 3.2.2
+asdf install ruby 3.4.5
+asdf global ruby 3.4.5
